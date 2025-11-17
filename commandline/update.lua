33b90208618ace0d8e4b1645a8713c2475cb0346
Leo <61291253+AnnikenYT@users.noreply.github.com>
2025-11-18 00:20:44 +0100

    Update commandline/update.lua

--- commandline/update.lua
+++ commandline/update.lua
@@ -1,8 +1,21 @@
 -- update from github and run
 
-local url = "https://raw.githubusercontent.com/AnnikenYT/cc-mek-fusion/refs/heads/main/commandline/cmd.lua"
-local filename = "cmd.lua"
+local executable = "cmd.lua"
 
-fs.delete(filename)
-shell.execute("wget", url, filename)
-shell.run(filename)
+local urls = {
+{
+    url = "https://raw.githubusercontent.com/AnnikenYT/cc-mek-fusion/refs/heads/main/commandline/cmd.lua",
+    filename = "cmd.lua"
+},
+{
+    url = "https://raw.githubusercontent.com/Ictoan42/CCLogger/refs/heads/main/CCLogger.lua",
+    filename = "CCLogger.lua"
+}
+}
+
+for _, file in ipairs(urls) do
+    fs.delete(file.filename)
+    shell.execute("wget", file.url, file.filename)
+end
+
+shell.run(executable)
