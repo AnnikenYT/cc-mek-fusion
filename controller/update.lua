a330c7fbab568d71947e4fa6f75bc1540af7b944
Leo <61291253+AnnikenYT@users.noreply.github.com>
2025-11-18 00:21:28 +0100

    Update controller/update.lua

--- controller/update.lua
+++ controller/update.lua
@@ -1,8 +1,21 @@
 -- update from github and run
 
-local url = "https://raw.githubusercontent.com/AnnikenYT/cc-mek-fusion/refs/heads/main/controller/controller.lua"
-local filename = "controller.lua"
+local executable = "controller.lua"
 
-fs.delete(filename)
-shell.execute("wget", url, filename)
-shell.run(filename)
+local urls = {
+{
+    url = "https://raw.githubusercontent.com/AnnikenYT/cc-mek-fusion/refs/heads/main/controller/controller.lua",
+    filename = "controller.lua"
+},
+{
+    url = "https://raw.githubusercontent.com/Ictoan42/CCLogger/refs/heads/main/CCLogger.lua",
+    filename = "CCLogger.lua"
+}
+}
+
+for _, file in ipairs(urls) do
+    fs.delete(file.filename)
+    shell.execute("wget", file.url, file.filename)
+end
+
+shell.run(executable)
